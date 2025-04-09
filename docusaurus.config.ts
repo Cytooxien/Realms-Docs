c7bc94ee4884dcb34ff7780c9716a4bd54078442
Dannyx <dannyx@dannyx.de>
2025-04-09 16:06:45 +0200

    Started adding the home page TODO: translate home page, make links on home page work

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,112 +3,129 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Cytooxien-Realms Wiki',
-  tagline: 'The official wiki for Cytooxien Realms',
-  favicon: 'favicon.png',
+    title: 'Cytooxien-Realms Wiki',
+    tagline: 'The official wiki for Cytooxien Realms',
+    favicon: 'favicon.png',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+    // Set the production url of your site here
+    url: 'https://your-docusaurus-site.example.com',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
 
-  organizationName: 'Cytooxien',
-  projectName: 'Realms-Docs',
+    organizationName: 'Cytooxien',
+    projectName: 'Realms-Docs',
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'de'],
-    localeConfigs: {
-      en: {
-        htmlLang: 'en-US',
-        label: 'English'
-      },
-      de: {
-        htmlLang: 'de',
-        label: 'Deutsch'
-      }
-    }
-  },
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'de'],
+        localeConfigs: {
+            en: {
+                htmlLang: 'en-US',
+                label: 'English'
+            },
+            de: {
+                htmlLang: 'de',
+                label: 'Deutsch'
+            }
+        }
+    },
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          editUrl:
-            'https://github.com/Cytooxien/Realms-Docs/tree/main/',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    editUrl:
+                        'https://github.com/Cytooxien/Realms-Docs/tree/main/',
+                },
+                theme: {
+                    customCss: './src/css/global.css',
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'Cy_T_14x.png',
-    navbar: {
-      title: 'Cytooxien-Realms Wiki',
-      logo: {
-        alt: 'Cytooxien Logo',
-        src: 'XLogo_3D_14x.png',
-      },
-      items: [
-        {
-          href: 'https://github.com/Cytooxien/Realms-Docs',
-          label: 'GitHub',
-          position: 'right',
-        },
-        {
-          type: 'localeDropdown',
-          position: 'left',
-        },
-      ],
-    },
-    footer: {
-      style: 'light',
-      links: [
-        {
-          title: 'Docs',
-          items: [
+    plugins: [
+        [
+            '@docusaurus/plugin-ideal-image',
             {
-              label: 'Getting Started',
-              to: '/docs/getting-started',
+                quality: 70,
+                max: 1030,
+                min: 640,
+                steps: 2,
+                disableInDev: false,
             },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Cytooxien Discord',
-              href: 'https://discord.gg/cytooxien',
+        ],
+    ],
+
+    themeConfig: {
+        image: 'Cy_T_14x.png',
+        navbar: {
+            title: 'Cytooxien-Realms Wiki',
+            logo: {
+                alt: 'Cytooxien Logo',
+                src: 'XLogo_3D_14x.png',
             },
-          ],
+            items: [
+                {
+                    type: 'html',
+                    position: 'right',
+                    value: '<a class="navbar__item_discord" href="https://discord.gg/cytooxien"><img src="/discord-mark-white.svg" alt="Discord icon" class="navbar__item_discord_img"></a>'
+                },
+                {
+                    href: 'https://github.com/Cytooxien/Realms-Docs',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+                {
+                    type: 'localeDropdown',
+                    position: 'left',
+                },
+            ],
         },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'GitHub',
-              href: 'https://github.com/Cytooxien/Realms-Docs',
-            },
-          ],
+        footer: {
+            style: 'light',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Getting Started',
+                            to: '/docs/getting-started',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Cytooxien Discord',
+                            href: 'https://discord.gg/cytooxien',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/Cytooxien/Realms-Docs',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Cytooxien. Built with Docusaurus.`,
         },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Cytooxien. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,75 @@
-import type {ReactNode} from 'react';
+import { useEffect, useState } from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Link from "@docusaurus/Link";
+import Translate from "@docusaurus/core/lib/client/exports/Translate";
 import Layout from '@theme/Layout';
+// @ts-ignore
+import Image from '@theme/IdealImage';
 
-export default function Home(): ReactNode {
-    const {siteConfig} = useDocusaurusContext();
+import '../css/homepage.css';
+
+const bgImages = [
+    './img/bg/1.png',
+    './img/bg/2.png',
+    './img/bg/3.png',
+    './img/bg/4.png',
+    './img/bg/5.png',
+    './img/bg/6.png',
+    './img/bg/7.png',
+    './img/bg/8.png',
+    './img/bg/9.png',
+    './img/bg/10.png'
+];
+
+export default function Home() {
+    const { siteConfig } = useDocusaurusContext();
+    const [bgIndex, setBgIndex] = useState(0);
+    const [prevBgIndex, setPrevBgIndex] = useState(null);
+    const [fade, setFade] = useState(true);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setPrevBgIndex(bgIndex);
+            setBgIndex(prev => (prev + 1) % bgImages.length);
+            setFade(true);
+
+            setTimeout(() => {
+                setFade(false);
+            }, 3000);
+        }, 15000);
+
+        return () => clearInterval(interval);
+    }, [bgIndex]);
+
     return (
         <Layout title="Cytooxien-Realms Wiki" description="This is the official documentation for Cytooxien Realms">
-            <h1>Cytooxien-Realms Wiki</h1>
-            <h2>Coming soon™</h2>
+            <main className={"main dark-theme"}>
+                <div>
+                    {prevBgIndex !== null && (
+                        <Image
+                            key={prevBgIndex}
+                            img={bgImages[prevBgIndex]}
+                            alt={"Previous Background"}
+                            className={`background-img ${fade ? 'fade-out' : 'hidden'}`}
+                        />
+                    )}
+                    <Image
+                        key={bgIndex}
+                        img={bgImages[bgIndex]}
+                        alt={"Current Background"}
+                        className={"background-img fade-in"}
+                    />
+                </div>
+
+                <div className={"card"}>
+                    <Image img={"/XLogo_3D_14x.png"} alt={"Cytooxien Logo"} className={"logo"}/>
+                    <h1 className={"title red-text"}>Cytooxien-Realms Wiki</h1>
+                    <p className={"subtitle"}>
+                        Welcome to the official documentation for Cytooxien Realms!
+                    </p>
+                    <a href={"./docs/getting-started"} className={"btn"}>Get Started</a>
+                </div>
+            </main>
         </Layout>
     );
 }
