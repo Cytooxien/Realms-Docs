55ddd3c6e23d616a46527ed8c94200e947a1db5e
Dannyx <dannyx@dannyx.de>
2025-05-10 23:35:03 +0200

    fixed background image preloading for main page, cleaned up images, fixed a few css issues TODO: proper css for FallbackImage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,30 +4,38 @@ import Layout from '@theme/Layout';
 // @ts-ignore
 import Image from '@theme/IdealImage';
 
-import BackgroundImages from "@site/src/components/bg-image";
-
 import '../css/homepage.css';
-import {useState} from "react";
+import React, {useState, useEffect} from "react";
+
+import BackgroundImages, {loadImages} from "@site/src/components/bg-image";
+import FallbackImage from "@site/src/components/fallback-image";
 
 export default function Home() {
     const { siteConfig } = useDocusaurusContext();
+    const [isLoaded, setIsLoaded] = useState(false);
 
-    const [background, setBackground] = useState(false);
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            loadImages().then(() => setIsLoaded(true));
+        }, 5000);
+
+        return () => clearTimeout(timeout);
+    }, [isLoaded]);
 
     return (
         <Layout title="Cytooxien-Realms Wiki" description="This is the community-driven documentation for Cytooxien Realms">
             <main className={"main"}>
-                {
-                    background ?
-                        <div className={"background-container"}>
-                            <BackgroundImages/>
-                        </div>
-                        :
-                        <></>
-                }
+                <div className={"background-container"}>
+                    {
+                        isLoaded ? (<BackgroundImages/>) : null
+                    }
+                    <div className={"background-img__svg"} style={{opacity: `${isLoaded ? 0 : 0.75}`}}>
+                        <FallbackImage whenLoaded={() => setIsLoaded(true)}/>
+                    </div>
+                </div>
 
                 <div className={"main__card"}>
-                    <Image img={"XLogo_3D_14x.webp"} alt={"Cytooxien Logo"} className={"logo"} onLoad={() => setBackground(true)} />
+                    <Image img={"XLogo_3D_14x.webp"} alt={"Cytooxien Logo"} className={"logo"}/>
                     <h1 className={"title red-text"}>Cytooxien-Realms Wiki</h1>
                     <p className={"subtitle"}>
                         Welcome to the community-driven documentation for Cytooxien Realms!
