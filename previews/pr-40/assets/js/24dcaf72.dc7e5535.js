e5e58eafc1197da2a99cf2fbce881cfbfbec7952
DerMistkaefer <DerMistkaefer@users.noreply.github.com>
2026-04-20 19:36:16 +0000

    Remove preview for PR 40 🛬

--- previews/pr-40/assets/js/24dcaf72.dc7e5535.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkrealms_docs=globalThis.webpackChunkrealms_docs||[]).push([[1804],{2956(e){e.exports=JSON.parse('{"metadata":{"permalink":"/Realms-Docs/previews/pr-40/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":0,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- previews/pr-40/de/assets/js/7115ab54.3119a590.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkrealms_docs=globalThis.webpackChunkrealms_docs||[]).push([[7775],{4344(e){e.exports=JSON.parse('{"metadata":{"permalink":"/Realms-Docs/previews/pr-40/de/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":0,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
